4a33d20fbbba75668cee0356d65896470c1acd80
JohnnyLyimo <johnnylyimo@gmail.com>
2022-03-20 15:30:12 +0300

    Created new Stateful widget named DesktopLargeView

---
+++ lib/widgets/desktop_large.dart
@@ -0,0 +1,13 @@
+class DesktopLargeView extends StatefulWidget {
+  const DesktopLargeView({Key? key}) : super(key: key);
+
+  @override
+  State<DesktopLargeView> createState() => _DesktopLargeViewState();
+}
+
+class _DesktopLargeViewState extends State<DesktopLargeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

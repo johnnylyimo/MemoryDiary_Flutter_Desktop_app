408f33dd13a8aa131bd9245a5d7e0791f34f52c0
JohnnyLyimo <johnnylyimo@gmail.com>
2022-03-20 15:32:23 +0300

    Created new Stateful widget named DesktopSmallView

---
+++ lib/widgets/desktop_small.dart
@@ -0,0 +1,13 @@
+class DesktopSmallView extends StatefulWidget {
+  const DesktopSmallView({Key? key}) : super(key: key);
+
+  @override
+  State<DesktopSmallView> createState() => _DesktopSmallViewState();
+}
+
+class _DesktopSmallViewState extends State<DesktopSmallView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

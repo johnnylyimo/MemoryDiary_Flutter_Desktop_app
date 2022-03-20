b31938468ade5efc44723ec0daf92b0260e86f20
JohnnyLyimo <johnnylyimo@gmail.com>
2022-03-20 17:33:19 +0300

    Wrapped Container widget with Row widget

--- lib/widgets/desktop_large.dart
+++ lib/widgets/desktop_large.dart
@@ -11,19 +11,23 @@ class _DesktopLargeViewState extends State<DesktopLargeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.all(20),
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: Colors.purple.shade200,
-        ),
-        child: Column(
-          children: [
-            RichText(
-              text: const TextSpan(text: 'WORD OF THE DAY\n'),
+      body: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.purple.shade200,
             ),
-          ],
-        ),
+            child: Column(
+              children: [
+                RichText(
+                  text: const TextSpan(text: 'WORD OF THE DAY\n'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }

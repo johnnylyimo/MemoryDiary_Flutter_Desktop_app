0b1e54ee1915bcdfa26b0edf72033ffcc55758f8
JohnnyLyimo <johnnylyimo@gmail.com>
2022-03-21 09:04:38 +0300

    Defined MediaQuery's height size variable

--- lib/widgets/desktop_large.dart
+++ lib/widgets/desktop_large.dart
@@ -11,6 +11,7 @@ class _DesktopLargeViewState extends State<DesktopLargeView> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Row(
         children: [

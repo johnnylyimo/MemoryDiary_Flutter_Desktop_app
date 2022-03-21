533079b4c1d35fa2644a120391e080cd0ad6eaeb
JohnnyLyimo <johnnylyimo@gmail.com>
2022-03-21 09:31:25 +0300

    Initialized Text widget with string value

--- lib/widgets/desktop_large.dart
+++ lib/widgets/desktop_large.dart
@@ -41,8 +41,7 @@ class _DesktopLargeViewState extends State<DesktopLargeView> {
                   height: 20.0,
                 ),
                 const Text(
-
-                ),
+                    'Anytime something positive happens, make a note of it and come back to it later.'),
               ],
             ),
           ),

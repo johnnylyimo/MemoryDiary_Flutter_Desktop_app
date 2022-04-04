1af38c374c80ecf625fee0e92a9b3442dc24e25e
JohnnyLyimo <johnnylyimo@gmail.com>
2022-04-04 09:47:07 +0300

    Imported hive_flutter package

--- lib/widgets/desktop_large.dart
+++ lib/widgets/desktop_large.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class DesktopLargeView extends StatefulWidget {
   const DesktopLargeView({Key? key}) : super(key: key);

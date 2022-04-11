1ddf2ad333e66966c81350b6f4cfe10773c2f8ce
JohnnyLyimo <johnnylyimo@gmail.com>
2022-04-11 12:53:35 +0300

    Defined  InputDecoration's prefixIcon with Icon widget

--- lib/widgets/desktop_large.dart
+++ lib/widgets/desktop_large.dart
@@ -41,8 +41,9 @@ class _DesktopLargeViewState extends State<DesktopLargeView> {
                       textAlign: TextAlign.center,
                       validator: (String? val) =>
                           val!.isNotEmpty ? null : 'Enter Memory',
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                         labelText: 'Edit your Memory below',
+                        prefixIcon: Icon(),
                       ),
                     ),
                   ],

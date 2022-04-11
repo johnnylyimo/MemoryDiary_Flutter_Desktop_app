ce51ccf941ef92daeb40b1714b54f37fb52a7e53
JohnnyLyimo <johnnylyimo@gmail.com>
2022-04-11 13:16:44 +0300

    Initialized OutlineInputBorder's borderSide with BorderSide widget

--- lib/widgets/desktop_large.dart
+++ lib/widgets/desktop_large.dart
@@ -61,6 +61,7 @@ class _DesktopLargeViewState extends State<DesktopLargeView> {
                           borderRadius: BorderRadius.all(
                             Radius.circular(10.0),
                           ),
+                          borderSide: BorderSide(),
                         ),
                       ),
                     ),

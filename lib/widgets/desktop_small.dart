a8797079143770f64894670825e29c3d05c6fdbd
JohnnyLyimo <johnnylyimo@gmail.com>
2022-04-30 19:36:48 +0300

    Defined SizedBox's height

--- lib/widgets/desktop_small.dart
+++ lib/widgets/desktop_small.dart
@@ -37,7 +37,9 @@ class _DesktopSmallViewState extends State<DesktopSmallView> {
                 ],
               ),
             ),
-            const SizedBox(),
+            const SizedBox(
+              height: 20.0,
+            ),
           ],
         ),
       ),

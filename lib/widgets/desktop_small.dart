05d35ba0f9f525207d0cf3b22bde3bd9a8a3dc8f
JohnnyLyimo <johnnylyimo@gmail.com>
2022-04-30 19:27:53 +0300

    Defined Container padding all sides

--- lib/widgets/desktop_small.dart
+++ lib/widgets/desktop_small.dart
@@ -15,6 +15,7 @@ class _DesktopSmallViewState extends State<DesktopSmallView> {
       body: Container(
         width: screenWidth,
         margin: const EdgeInsets.all(20),
+        padding: const EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [

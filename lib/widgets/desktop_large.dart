690ecda3568869b43f5b2c2fe391cd0d1350c66e
JohnnyLyimo <johnnylyimo@gmail.com>
2022-05-09 03:16:37 +0300

    Defined ElevatedButton's styleForm shape property

--- lib/widgets/desktop_large.dart
+++ lib/widgets/desktop_large.dart
@@ -193,6 +193,7 @@ class _DesktopLargeViewState extends State<DesktopLargeView> {
                         style: ElevatedButton.styleFrom(
                           padding: EdgeInsets.all(screenWidth * .01),
                           primary: Colors.purple.shade500,
+                          shape: const StadiumBorder(),
                         ),
                         onPressed: () {
                           if (_formKey.currentState!.validate()) {

ab1c0cf628f51816ad77a8f5943aabfb4e38838d
JohnnyLyimo <johnnylyimo@gmail.com>
2022-05-21 19:16:06 +0300

    Defined Text's string value

--- lib/widgets/desktop_large.dart
+++ lib/widgets/desktop_large.dart
@@ -89,7 +89,9 @@ class _DesktopLargeViewState extends State<DesktopLargeView> {
                             Navigator.pop(context);
                             ScaffoldMessenger.of(context).showSnackBar(
                               SnackBar(
-                                content: Text(),
+                                content: Text(
+                                  'The memory " ${editMemoryController.text.substring(0, editMemoryController.text.length)} " Already been Deleted!',
+                                ),
                               ),
                             );
                           },

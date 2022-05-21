898921c04e2bbd81762def86123958c48c8dc0c2
JohnnyLyimo <johnnylyimo@gmail.com>
2022-05-21 19:40:01 +0300

    Initialized memory argument with editMemoryController

--- lib/widgets/desktop_small.dart
+++ lib/widgets/desktop_small.dart
@@ -18,7 +18,9 @@ class _DesktopSmallViewState extends State<DesktopSmallView> {
   // final _editFormKey = GlobalKey<FormState>();
   Box? box;
 
-  editDialog(String memory, int memoryKey) {}
+  editDialog(String memory, int memoryKey) {
+    editMemoryController.text = memory;
+  }
 
   @override
   void initState() {

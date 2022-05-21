6f8c74040435e76fea3bb77a4ef948a17682d3b5
JohnnyLyimo <johnnylyimo@gmail.com>
2022-05-21 19:06:14 +0300

    Invoked ScaffoldMessenger's showSnackBar

--- lib/widgets/desktop_large.dart
+++ lib/widgets/desktop_large.dart
@@ -87,6 +87,7 @@ class _DesktopLargeViewState extends State<DesktopLargeView> {
                           onPressed: () {
                             box!.delete(memoryKey);
                             Navigator.pop(context);
+                            ScaffoldMessenger.of(context).showSnackBar();
                           },
                         ),
                         const SizedBox(

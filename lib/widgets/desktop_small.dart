b347fe5c3d120400f3f0c4dfa00055066c8e7f59
JohnnyLyimo <johnnylyimo@gmail.com>
2022-05-21 19:32:53 +0300

    Invoked editDialog method from IconButton onPressed method

--- lib/widgets/desktop_small.dart
+++ lib/widgets/desktop_small.dart
@@ -186,7 +186,7 @@ class _DesktopSmallViewState extends State<DesktopSmallView> {
                                 ),
                                 trailing: IconButton(
                                   icon: const Icon(Icons.edit),
-                                  onPressed: () {},
+                                  onPressed: () => editDialog(),
                                 ),
                               ),
                             );

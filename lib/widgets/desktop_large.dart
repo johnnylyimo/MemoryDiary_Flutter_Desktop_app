5f04929883b3d1c3297c5d08bfdf0d9ad908e239
JohnnyLyimo <johnnylyimo@gmail.com>
2022-05-21 18:49:10 +0300

    Added SizedBox widget to Row children

--- lib/widgets/desktop_large.dart
+++ lib/widgets/desktop_large.dart
@@ -75,6 +75,7 @@ class _DesktopLargeViewState extends State<DesktopLargeView> {
                     ),
                     Row(
                       children: [
+                        SizedBox(),
                         ElevatedButton(
                           child: const Text('Save'),
                           onPressed: () {

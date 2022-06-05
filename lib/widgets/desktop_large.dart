b87467ff05d2f2a85d979858081c2de0bf205835
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-05 16:06:32 +0300

    Defined Column's mainAxisAlignment property

--- lib/widgets/desktop_large.dart
+++ lib/widgets/desktop_large.dart
@@ -313,6 +313,7 @@ class _DesktopLargeViewState extends State<DesktopLargeView> {
                                 })
                             : Column(
                                 crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: const [
                                   Text(
                                       'No Memory\nAdd your memory on left side panel'),

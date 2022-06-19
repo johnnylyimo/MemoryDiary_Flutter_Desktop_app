d9d738c89c203ce684f18ab38e748a12931c9252
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 13:53:01 +0300

    Defined Dialog's child with Container widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -65,6 +65,7 @@ class _HomeState extends State<Home> {
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30),
                       ),
+                      child: Container(),
                     ),
                   );
               }

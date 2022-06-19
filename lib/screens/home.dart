7c7a0b42e4c09a9b679d949d5fc22cdd1b5838dd
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 15:23:21 +0300

    Defined ElevatedButton's onPressed method by invoked Navigator method

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -163,6 +163,7 @@ class _HomeState extends State<Home> {
                             ),
                             ElevatedButton(
                               child: const Text('Close'),
+                              onPressed: () => Navigator.pop(context),
                             ),
                           ],
                         ),

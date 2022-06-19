2181af08b5ce1eb05cc8726d8a71a52d65bcb7c7
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 13:16:02 +0300

    Defined TextStyle's color

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -36,7 +36,7 @@ class _HomeState extends State<Home> {
                   ),
                   label: const Text(
                     'Help',
-                    style: TextStyle(),
+                    style: TextStyle(color: Colors.black),
                   ),
                 ),
               ),

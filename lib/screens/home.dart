1ec01d4dab3a3302bcea0a659e444ad1dabcdf18
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 15:50:55 +0300

    Initialized Container's decoration with BoxDecoration

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -190,6 +190,7 @@ class _HomeState extends State<Home> {
                         height: MediaQuery.of(context).size.height * 0.7,
                         width: MediaQuery.of(context).size.width * 0.4,
                         padding: const EdgeInsets.all(20.0),
+                        decoration: BoxDecoration(),
                       ),
                     ),
                   );

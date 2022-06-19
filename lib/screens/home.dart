a574fc9495f53aa7ab85e9b95979e433e90ce560
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 15:25:00 +0300

    Initialized ElevatedButton's style with ElevatedButton. styleFrom

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -163,6 +163,7 @@ class _HomeState extends State<Home> {
                             ),
                             ElevatedButton(
                               child: const Text('Close'),
+                              style: ElevatedButton.styleFrom(),
                               onPressed: () => Navigator.pop(context),
                             ),
                           ],

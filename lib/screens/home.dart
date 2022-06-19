01ae48ad09184b7217ca3492104f98e1a6c5b462
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 14:26:19 +0300

    Defined TextStyle's color

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -113,6 +113,7 @@ class _HomeState extends State<Home> {
                                           text: "How to use MemoryDiary App\n",
                                           style: TextStyle(
                                             fontWeight: FontWeight.bold,
+                                            color: Colors.black,
                                           ),
                                         )
                                       ],

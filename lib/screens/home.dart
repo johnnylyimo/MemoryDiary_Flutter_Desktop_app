9ac09641e589f1f460fc6a848c59c832631c2c11
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 14:19:25 +0300

    Defined TextStyle's fontSize

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -97,6 +97,7 @@ class _HomeState extends State<Home> {
                                       style: TextStyle(
                                         fontWeight: FontWeight.bold,
                                         color: Colors.black,
+                                        fontSize: 18.0,
                                       ),
                                     ),
                                   ),

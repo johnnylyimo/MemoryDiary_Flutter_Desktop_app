c5f6ec0a476c4105c36bba28fc5bb2d3a3fe03a3
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 13:23:50 +0300

    Defined TextStyle's color

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -50,7 +50,7 @@ class _HomeState extends State<Home> {
                   ),
                   label: const Text(
                     'About MemoryDiary',
-                    style: TextStyle(),
+                    style: TextStyle(color: Colors.black),
                   ),
                 ),
               ),

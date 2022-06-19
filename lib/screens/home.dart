cb5f02ef681a10d3639978a3a9ab7b72c101bf99
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 15:50:04 +0300

    Defined Container's padding

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -189,6 +189,7 @@ class _HomeState extends State<Home> {
                       child: Container(
                         height: MediaQuery.of(context).size.height * 0.7,
                         width: MediaQuery.of(context).size.width * 0.4,
+                        padding: const EdgeInsets.all(20.0),
                       ),
                     ),
                   );

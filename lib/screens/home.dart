83a78ef074ee6cd169f8693f4e76204184d704c0
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 15:21:42 +0300

    Added ElevatedButton widget to Column's children

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -161,6 +161,7 @@ class _HomeState extends State<Home> {
                                 fontSize: 13.0,
                               ),
                             ),
+                            ElevatedButton(),
                           ],
                         ),
                       ),

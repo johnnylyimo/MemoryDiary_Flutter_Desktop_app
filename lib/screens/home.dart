a48defa13ea1a6ecf4f30a6aeff021b352c33d44
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 14:52:02 +0300

    Defined TextStyle's color

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -132,6 +132,7 @@ class _HomeState extends State<Home> {
                                           text: 'Features\n',
                                           style: TextStyle(
                                             fontWeight: FontWeight.bold,
+                                            color: Colors.black,
                                           ),
                                         )
                                       ],

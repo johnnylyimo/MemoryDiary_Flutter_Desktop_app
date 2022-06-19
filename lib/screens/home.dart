998340e8cb32bcd9a52b5f8b6e3c071ad802995a
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 13:23:31 +0300

    Initialized Text's style with TextStyle widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -50,6 +50,7 @@ class _HomeState extends State<Home> {
                   ),
                   label: const Text(
                     'About MemoryDiary',
+                    style: TextStyle(),
                   ),
                 ),
               ),

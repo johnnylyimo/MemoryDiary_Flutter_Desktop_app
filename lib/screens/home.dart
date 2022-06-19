e0d762c3ba2e52b7931af21eb683ad12b30a873f
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 15:36:04 +0300

    Defined Expanded's child with SizedBox

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -162,7 +162,7 @@ class _HomeState extends State<Home> {
                                 fontSize: 13.0,
                               ),
                             ),
-                            const Expanded(),
+                            const Expanded(child: SizedBox()),
                             ElevatedButton(
                               child: const Text('Close'),
                               style: ElevatedButton.styleFrom(

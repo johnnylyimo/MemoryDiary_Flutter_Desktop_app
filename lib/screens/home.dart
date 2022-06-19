3dbb60f555bb492bf37f6268d0a606e7af0595e4
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 14:25:31 +0300

    Initialized TextSpan's style with TextStyle widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -111,6 +111,7 @@ class _HomeState extends State<Home> {
                                         ),
                                         TextSpan(
                                           text: "How to use MemoryDiary App\n",
+                                          style: TextStyle(),
                                         )
                                       ],
                                     ),

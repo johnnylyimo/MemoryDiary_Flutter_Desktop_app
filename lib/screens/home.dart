a2aa86a15305bd2fd2d47d33e9a570c507d633ef
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 15:34:52 +0300

    Updated Container's height

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -66,7 +66,7 @@ class _HomeState extends State<Home> {
                         borderRadius: BorderRadius.circular(30),
                       ),
                       child: Container(
-                        height: MediaQuery.of(context).size.height * 0.6,
+                        height: MediaQuery.of(context).size.height * 0.7,
                         width: MediaQuery.of(context).size.width * 0.4,
                         padding: const EdgeInsets.all(20.0),
                         decoration: BoxDecoration(

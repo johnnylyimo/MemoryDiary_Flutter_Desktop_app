21179f98dd0f300bd588bcdd20b6c5a753faf067
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 13:56:21 +0300

    Defined BoxDecoration's borderRadius

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -71,6 +71,7 @@ class _HomeState extends State<Home> {
                         padding: const EdgeInsets.all(20.0),
                         decoration: BoxDecoration(
                           color: Colors.purple.shade100,
+                          borderRadius: BorderRadius.circular(30),
                         ),
                       ),
                     ),

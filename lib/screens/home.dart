54499fe31011751c802aead2cc78f1286be17045
JohnnyLyimo <johnnylyimo@gmail.com>
2022-06-19 13:59:42 +0300

    Defined Container child with Column widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -73,6 +73,7 @@ class _HomeState extends State<Home> {
                           color: Colors.purple.shade100,
                           borderRadius: BorderRadius.circular(30),
                         ),
+                        child: Column(),
                       ),
                     ),
                   );

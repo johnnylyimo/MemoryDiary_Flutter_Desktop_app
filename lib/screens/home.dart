f31bb9f9cf7d752adddc850bc1beb7a95948b5aa
johnnylyimo <johnnylyimo@gmail.com>
2022-06-27 02:49:23 +0300

    Defined Text's string value

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -296,7 +296,7 @@ class _HomeState extends State<Home> {
                             ),
                             const Expanded(child: SizedBox()),
                             ElevatedButton(
-                              child: const Text(),
+                              child: Text('Close'),
                             ),
                           ],
                         ),

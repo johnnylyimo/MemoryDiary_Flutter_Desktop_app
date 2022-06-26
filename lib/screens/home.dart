79b77590800d66e2bfe704a4fefb6f7ff125de8f
johnnylyimo <johnnylyimo@gmail.com>
2022-06-27 02:15:57 +0300

    Initialized SingleChildScrollView's child with RichText widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -250,6 +250,7 @@ class _HomeState extends State<Home> {
                                   scrollDirection: Axis.vertical,
                                   padding:
                                       EdgeInsets.symmetric(horizontal: 10.0),
+                                  child: RichText(),
                                 ),
                               ),
                             )

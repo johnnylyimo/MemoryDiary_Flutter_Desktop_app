1af6e02e9e103c24fbdce385d589c52eda08dc6b
johnnylyimo <johnnylyimo@gmail.com>
2022-06-27 02:11:46 +0300

    Initialized Scrollbar's child with SingleChildScrollView widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -246,6 +246,7 @@ class _HomeState extends State<Home> {
                               child: Scrollbar(
                                 thumbVisibility: true,
                                 thickness: 10.0,
+                                child: SingleChildScrollView(),
                               ),
                             )
                           ],

286fba75731c1f09cea2c15851bb084f72feb35b
johnnylyimo <johnnylyimo@gmail.com>
2022-06-27 02:12:57 +0300

    Defined SingleChildScrollView's scrollDirection

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -246,7 +246,9 @@ class _HomeState extends State<Home> {
                               child: Scrollbar(
                                 thumbVisibility: true,
                                 thickness: 10.0,
-                                child: SingleChildScrollView(),
+                                child: SingleChildScrollView(
+                                  scrollDirection: Axis.vertical,
+                                ),
                               ),
                             )
                           ],

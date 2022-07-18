361ba6b4d4f2eae96b4dc729a26eb67cf1a876b6
johnnylyimo <johnnylyimo@gmail.com>
2022-07-18 10:13:31 +0300

    Passed parameter   enableDomStorage set to false

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -20,6 +20,7 @@ class _HomeState extends State<Home> {
         useSafariVC: false,
         useWebView: false,
         enableJavaScript: false,
+        enableDomStorage: false,
       )
     }
   }
